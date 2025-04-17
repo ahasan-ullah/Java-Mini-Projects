e343270fdb4b50d84df6a3dd285c04b179076dd0
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-17 23:24:47 +0600

    askQuestion method declared in the encryption program

--- src/encryption/program/EncryptionProgram.java
+++ src/encryption/program/EncryptionProgram.java
@@ -24,7 +24,32 @@ public class EncryptionProgram {
         askQuestion();
     }
     private void askQuestion(){
+        while(true){
+            System.out.println("*****************************************************");
+            System.out.println("What do you want to do?");
+            System.out.println("(N)ewKey, (G)etKey, (E)ncrypt, (D)ecrypt, (Q)uit");
+            char response=Character.toUpperCase(scanner.nextLine().charAt(0));
 
+            switch(response){
+                case 'N':
+                    newKey();
+                    break;
+                case 'G':
+                    getKey();
+                    break;
+                case 'E':
+                    encrypt();
+                    break;
+                case 'D':
+                    decrypt();
+                    break;
+                case 'q':
+                    quit();
+                    break;
+                default:
+                    System.out.println("Not a valid answer :(");
+            }
+        }
     }
     private void newKey(){
 
