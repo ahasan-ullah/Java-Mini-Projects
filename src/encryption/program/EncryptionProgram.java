fa35c82f33e1b991af965073e69828eef11a5c6b
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-17 22:38:18 +0600

    all the necessary methods added to the encryption program class

--- src/encryption/program/EncryptionProgram.java
+++ src/encryption/program/EncryptionProgram.java
@@ -4,4 +4,22 @@ public class EncryptionProgram {
     public EncryptionProgram(){
 
     }
+    private void askQuestion(){
+
+    }
+    private void newKey(){
+
+    }
+    private void getKey(){
+
+    }
+    private void encrypt(){
+
+    }
+    private void decrypt(){
+
+    }
+    private void quit(){
+        
+    }
 }
