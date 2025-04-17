961c185a481078cdafbd02b0cd66483eb10af3bd
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-17 23:56:39 +0600

    decrypt method declared in the encryption program

--- src/encryption/program/EncryptionProgram.java
+++ src/encryption/program/EncryptionProgram.java
@@ -95,7 +95,24 @@ public class EncryptionProgram {
         System.out.println();
     }
     private void decrypt(){
+        System.out.println("Enter a message to be encrypted");
+        String message=scanner.nextLine();
 
+        letters=message.toCharArray();
+
+        for(int i=0;i<letters.length;i++){
+            for(int j=0;j<shuffledList.size();j++){
+                if(letters[i]==shuffledList.get(j)){
+                    letters[i]=list.get(j);
+                    break;
+                }
+            }
+        }
+        System.out.println("Decrypted: ");
+        for(char x:letters){
+            System.out.print(x);
+        }
+        System.out.println();
     }
     private void quit(){
 
