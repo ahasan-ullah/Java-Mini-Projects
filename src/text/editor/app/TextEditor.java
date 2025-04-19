edc04a2ba675e3f2b7c35cee7274238b748f154f
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-19 09:13:06 +0600

    text editor basic frame added

--- src/text/editor/app/TextEditor.java
+++ src/text/editor/app/TextEditor.java
@@ -1,7 +1,18 @@
 package text.editor.app;
 
-public class TextEditor {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TextEditor extends JFrame implements ActionListener {
     public TextEditor(){
-        System.out.println("HI");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Text Editor");
+        this.setSize(500, 500);
+        this.setLayout(new FlowLayout());
+        this.setVisible(true);
     }
+    @Override
+    public void actionPerformed(ActionEvent e){}
 }
