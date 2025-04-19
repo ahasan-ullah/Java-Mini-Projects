26a4e0028541cd2588ad6272cf2693f16769cd31
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-19 09:28:26 +0600

    scrollpane added to the text editor app to add vertical scrollbar

--- src/text/editor/app/TextEditor.java
+++ src/text/editor/app/TextEditor.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 
 public class TextEditor extends JFrame implements ActionListener {
     private JTextArea textArea;
+    private JScrollPane scrollPane;
     public TextEditor(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Text Editor");
@@ -15,12 +16,16 @@ public class TextEditor extends JFrame implements ActionListener {
         this.setLocationRelativeTo(null);
 
         textArea=new JTextArea();
-        textArea.setPreferredSize(new Dimension(650,550));
+        //textArea.setPreferredSize(new Dimension(650,550));
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
         textArea.setFont(new Font("Arial",Font.PLAIN,20));
 
-        this.add(textArea);
+        scrollPane=new JScrollPane(textArea);
+        scrollPane.setPreferredSize(new Dimension(650, 550));
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        this.add(scrollPane);
         this.setVisible(true);
     }
     @Override
