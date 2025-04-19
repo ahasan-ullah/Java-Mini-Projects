6545285057f2dd4e664ff028b09b15e528e1c80a
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-19 09:19:46 +0600

    text area added to the text editor app

--- src/text/editor/app/TextEditor.java
+++ src/text/editor/app/TextEditor.java
@@ -6,11 +6,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TextEditor extends JFrame implements ActionListener {
+    private JTextArea textArea;
     public TextEditor(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Text Editor");
         this.setSize(500, 500);
         this.setLayout(new FlowLayout());
+        this.setLocationRelativeTo(null);
+
+        textArea=new JTextArea();
+        textArea.setPreferredSize(new Dimension(450,450));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+
+        this.add(textArea);
         this.setVisible(true);
     }
     @Override
