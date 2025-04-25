e642e7321b2c37824bd5c6f8b97614e922be250a
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-26 00:39:12 +0600

    timeLabel added to show the current time in the clock app

--- src/clock/app/MyFrame.java
+++ src/clock/app/MyFrame.java
@@ -2,14 +2,30 @@ package clock.app;
 
 import javax.swing.*;
 import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MyFrame extends JFrame {
+    private Calendar calendar;
+    private SimpleDateFormat timeFormat;
+    private JLabel timeLabel;
+    String time;
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Clock");
         this.setLayout(new FlowLayout());
         this.setSize(350, 200);
         this.setResizable(false);
+
+        timeFormat=new SimpleDateFormat("hh:mm:ss a");
+
+        timeLabel=new JLabel();
+        time=timeFormat.format(Calendar.getInstance().getTime());
+        timeLabel.setText(time);
+
+
+
+        this.add(timeLabel);
         this.setVisible(true);
     }
 }
