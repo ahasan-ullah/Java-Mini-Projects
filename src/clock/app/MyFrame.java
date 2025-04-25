ba1002bd030c8ee9685879670bd5ba3e17632e4e
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-26 00:34:11 +0600

    basic frame setup done

--- src/clock/app/MyFrame.java
+++ src/clock/app/MyFrame.java
@@ -1,4 +1,15 @@
 package clock.app;
 
-public class MyFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Clock");
+        this.setLayout(new FlowLayout());
+        this.setSize(350, 200);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
 }
