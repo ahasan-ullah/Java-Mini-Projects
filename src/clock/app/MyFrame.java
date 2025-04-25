53c9a79472ad7287241bf5e9337754a83240fd48
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-26 00:47:27 +0600

    font, color and background color off the clock app chnaged

--- src/clock/app/MyFrame.java
+++ src/clock/app/MyFrame.java
@@ -20,7 +20,10 @@ public class MyFrame extends JFrame {
         timeFormat=new SimpleDateFormat("hh:mm:ss a");
 
         timeLabel=new JLabel();
-
+        timeLabel.setFont(new Font("Verdana", Font.PLAIN, 50));
+        timeLabel.setForeground(new Color(0x00FF00));
+        timeLabel.setBackground(Color.BLACK);
+        timeLabel.setOpaque(true);
 
 
         this.add(timeLabel);
