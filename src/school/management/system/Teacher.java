81cc442183b98aa25224c7ff7c07fa016e30a0da
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-27 12:37:19 +0600

    Teacher class added and also variable and constructor added

---
+++ src/school/management/system/Teacher.java
@@ -0,0 +1,19 @@
+package school.management.system;
+
+public class Teacher {
+    private int id;
+    private String name;
+    private int salary;
+
+    /**
+     * Creates a new teacher class object
+     * @param id id for the teacher
+     * @param name name of the teacher
+     * @param salary salary of the teacher
+     */
+    public Teacher(int id,String name,int salary){
+        this.id=id;
+        this.name=name;
+        this.salary=salary;
+    }
+}
