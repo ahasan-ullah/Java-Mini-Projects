130f00d43eacbbd679183091e515230832abbc2c
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-27 14:25:22 +0600

    setter and getter method added to school class for the school management system

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -17,4 +17,64 @@ public class School {
         this.teachers=teachers;
         this.students=students;
     }
+
+    /**
+     * @return the list of teachers of the school
+     */
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    /**
+     * @return the list of students of the school
+     */
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    /**
+     * @return the total money earned by the school
+     */
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    /**
+     * @return the total money spent by the school
+     */
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    /**
+     * adds the total money
+     * @param totalMoneySpent total money spent by the school
+     */
+    public void updateTotalMoneySpent(int totalMoneySpent) {
+        this.totalMoneyEarned-=totalMoneySpent;
+    }
+
+    /**
+     * adds total money earned
+     * @param totalMoneyEarned total money earned by the school
+     */
+    public void updateTotalMoneyEarned(int totalMoneyEarned) {
+        this.totalMoneyEarned += totalMoneyEarned;
+    }
+
+    /**
+     * Add a student to the school
+     * @param student the student to be added
+     */
+    public void addStudents(Student student) {
+        students.add(student);
+    }
+
+    /**
+     * Add a teacher to the school
+     * @param teacher the teacher to be added
+     */
+    public void addTeachers(Teacher teacher) {
+        teachers.add(teacher);
+    }
 }
