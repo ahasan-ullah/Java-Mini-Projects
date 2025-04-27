15e65c466974431e500ce4c421c971f355a2458d
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-27 14:15:05 +0600

    teacher class created and variable and consttructor created

---
+++ src/school/management/system/School.java
@@ -0,0 +1,20 @@
+package school.management.system;
+
+import java.util.List;
+
+public class School {
+    private List<Teacher> teachers;
+    private List<Student>students;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
+
+    /**
+     * new school object is created
+     * @param teachers list of teachers in the school
+     * @param students list of students in the school
+     */
+    public School(List<Teacher>teachers,List<Student>students){
+        this.teachers=teachers;
+        this.students=students;
+    }
+}
