a0bce3941cc49669f9d6f68c1497953ddb3b2aa9
ahasan-ullah <ahasanullah451@gmail.com>
2025-04-27 12:41:33 +0600

    necessary setter and getter method added to teacher class in the student management system

--- src/school/management/system/Teacher.java
+++ src/school/management/system/Teacher.java
@@ -16,4 +16,33 @@ public class Teacher {
         this.name=name;
         this.salary=salary;
     }
+
+    /**
+     * @return the id of the teacher
+     */
+    public int getId(){
+        return id;
+    }
+
+    /**
+     * @return the name of the teacher
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * @return the salary of the teacher
+     */
+    public int getSalary(){
+        return salary;
+    }
+
+    /**
+     * set the salary
+     * @param salary salary of the teacher
+     */
+    public void setSalary(int salary){
+        this.salary=salary;
+    }
 }
